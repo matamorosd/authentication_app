c1c2c3f9208cce6f278e99ab8141d902f417b809
David Matamoros <davidmatamoros90@gmail.com>
2024-03-07 15:32:07 -0600

    add devise controllers to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, 
+    controllers: { 
+      sessions: 'users/sessions',
+      omniauth_callbacks: 'users/omniauth_callbacks' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
